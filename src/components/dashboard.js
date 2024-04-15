e5b3682cfd344d8a121b25a08a35dac29c9c03e7
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2024-04-15 16:43:40 +0300

    header complete

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,24 @@
 import './styles/dashboard.css';
-
+import brand from '../assets/images/agriconnect_logo.png';
+import contributors from '../assets/images/sponsor.svg';
 export default function Dashboard(){
     return (
         <div className='page'>
+            <header>
+                <div className="logos">
+                    <img src={brand} alt='Agriconnect'/>
+                    <img id='small-img' src={contributors} alt="Contributors"/>
+                </div>
+                <div className="routes">
+                    <button>Home</button>
+                    <button id='active'>Dashboard</button>
+                    <div className='user flex-center'>KK</div>
+                </div>
+            </header>
 
+            <section>
+
+            </section>
         </div>
     );
 }
